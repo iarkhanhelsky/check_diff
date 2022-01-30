85d2b4e3a3eb74009cbcbdc658e13fc22b2799b4
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-01-30 16:55:52 +0200

    kubelint: extract settings reader function

--- pkg/checker/k8s/kubelinter/module.go
+++ pkg/checker/k8s/kubelinter/module.go
@@ -1,15 +1,10 @@
 package kubelinter
 
 import (
-	"go.uber.org/config"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
-	fx.Provide(func(yaml *config.YAML) (Settings, error) {
-		v := Settings{}
-		err := yaml.Get("KubeLinter").Populate(&v)
-		return v, err
-	}),
+	fx.Provide(ReadSettings),
 	fx.Provide(NewK8KubeLint),
 )
--- pkg/checker/k8s/kubelinter/settings.go
+++ pkg/checker/k8s/kubelinter/settings.go
@@ -2,8 +2,15 @@ package kubelinter
 
 import (
 	"github.com/iarkhanhelsky/check_diff/pkg/core"
+	"go.uber.org/config"
 )
 
 type Settings struct {
 	core.Settings `yaml:",inline"`
+}
+
+func ReadSettings(yaml *config.YAML) (Settings, error) {
+	v := Settings{}
+	err := yaml.Get("KubeLinter").Populate(&v)
+	return v, err
 }
