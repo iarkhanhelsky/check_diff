dd1e0205571643095fca8d3709a4c85b80836cc0
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-09 23:16:33 +0200

    checker: generic provide method

--- pkg/checker/module.go
+++ pkg/checker/module.go
@@ -4,11 +4,24 @@ import (
 	"github.com/iarkhanhelsky/check_diff/pkg/checker/java/checkstyle"
 	"github.com/iarkhanhelsky/check_diff/pkg/checker/k8s/kubelinter"
 	"github.com/iarkhanhelsky/check_diff/pkg/checker/ruby/rubocop"
+	"github.com/iarkhanhelsky/check_diff/pkg/core"
+	"go.uber.org/config"
 	"go.uber.org/fx"
 )
 
-var Module = fx.Options(
-	fx.Provide(fx.Annotated{Group: "checkers", Target: kubelinter.NewKubeLint}),
-	fx.Provide(fx.Annotated{Group: "checkers", Target: checkstyle.NewCheckstyle}),
-	fx.Provide(fx.Annotated{Group: "checkers", Target: rubocop.NewRubocop}),
+type provider func(yaml *config.YAML) (core.Checker, error)
+
+var Module = ProvideCheckers(
+	kubelinter.NewKubeLint,
+	checkstyle.NewCheckstyle,
+	rubocop.NewRubocop,
 )
+
+func ProvideCheckers(checkers ...provider) fx.Option {
+	var annotated []interface{}
+	for _, ch := range checkers {
+		annotated = append(annotated, fx.Annotated{Group: "checkers", Target: ch})
+	}
+
+	return fx.Provide(annotated...)
+}
