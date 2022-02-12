178ff2940543e77c1bb14148ab7dded6d1b37c9a
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-12 13:49:45 +0200

    NewDiff logging

--- pkg/app/diff.go
+++ pkg/app/diff.go
@@ -4,16 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/iarkhanhelsky/check_diff/pkg/core"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
 	"io"
 	"os"
 )
 
-func NewDiff(cliOptions CliOptions) ([]core.LineRange, error) {
+type DiffParams struct {
+	fx.In
+
+	CliOptions CliOptions
+	Logger     *zap.Logger
+}
+
+func NewDiff(params DiffParams) ([]core.LineRange, error) {
 	var reader io.Reader
+
+	cliOptions := params.CliOptions
+	logger := params.Logger.Sugar()
 	if len(cliOptions.InputFile) == 0 {
+		logger.Debugf("reading diff from STDOUT")
 		reader = os.Stdin
 	} else {
 		file, err := os.Open(cliOptions.InputFile)
+		logger.With("file", cliOptions.InputFile).Debugf("reading diff from file")
 		if err != nil {
 			return nil, fmt.Errorf("can't read file: %s: %v", cliOptions.InputFile, err)
 		}
