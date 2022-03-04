d3b17918bed199a5c48cfbd1dba4fd622fe7bf68
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-03-04 07:52:13 +0200

    chore(sarif): add tests

--- pkg/mapper/sarif_test.go
+++ pkg/mapper/sarif_test.go
@@ -2,6 +2,7 @@ package mapper
 
 import (
 	"fmt"
+	"github.com/iarkhanhelsky/check_diff/pkg/core"
 	"github.com/owenrumney/go-sarif/sarif"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -20,10 +21,53 @@ func (s *SarifTestSuite) getBytes(file string) []byte {
 	return bytes
 }
 
+func (s *SarifTestSuite) TestParseSarif() {
+	testcases := map[string]struct {
+		report []byte
+		issues []core.Issue
+		err    bool
+	}{
+		"kube_linter_sarif_report.json": {
+			report: s.getBytes("testdata/kube_linter_sarif_report.json"),
+			issues: []core.Issue{
+				{Tag: "no-read-only-root-fs", File: "k8s/deployment.yaml",
+					Line: 1, Column: 0, Severity: "error",
+					Message: "container \"sec-ctx-demo\" does not have a read-only root file system\nobject: <no namespace>/security-context-demo /v1, Kind=Pod",
+					Source:  ""},
+				{Tag: "unset-cpu-requirements", File: "k8s/deployment.yaml",
+					Line: 1, Column: 0, Severity: "error",
+					Message: "container \"sec-ctx-demo\" has cpu limit 0\nobject: <no namespace>/security-context-demo /v1, Kind=Pod",
+					Source:  ""},
+				core.Issue{Tag: "unset-memory-requirements", File: "k8s/deployment.yaml",
+					Line: 1, Column: 0, Severity: "error",
+					Message: "container \"sec-ctx-demo\" has memory limit 0\nobject: <no namespace>/security-context-demo /v1, Kind=Pod",
+					Source:  ""}},
+		},
+
+		"bad report": {
+			report: []byte("_"),
+			err:    true,
+		},
+	}
+
+	for name, tc := range testcases {
+		s.T().Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			issues, err := parseSarif(tc.report)
+			if tc.err {
+				assert.Error(err)
+			} else {
+				assert.Equal(tc.issues, issues)
+			}
+		})
+	}
+}
+
 func (s *SarifTestSuite) TestExtractLocation() {
 	assert := s.Assert()
 
-	type testCase struct {
+	testcases := []struct {
 		report string
 		run    int
 		result int
@@ -31,9 +75,7 @@ func (s *SarifTestSuite) TestExtractLocation() {
 		line   int
 		column int
 		file   string
-	}
-
-	testcases := []testCase{
+	}{
 		{
 			report: "kube_linter_sarif_report.json", run: 0,
 			line: 1, column: 0, file: "k8s/deployment.yaml",
